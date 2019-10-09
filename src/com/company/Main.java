e83f4aff685c56f751519d787b9129a92e331d58
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2019-10-09 11:58:26 +0200

    blabla

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] integers = new int[args.length];
+        Int[] integers = new Int[args.length];
         for (int i = 0; i < args.length; ++i) {
 
             char[] number = args[i].toCharArray();
             int multiplier = 1;
+            integers[i] = new Int(0);
             for (int j = number.length - 1, power = 0; j >= 0; --j) {
 
                 if (!Character.isDigit(number[j]))
@@ -21,25 +22,24 @@ public class Main {
                 }
 
                 int digit = multiplier * (int)Math.pow(10, power) * (number[j] - '0');
-
-                integers[i] +=  digit;
+                integers[i].setValue(integers[i].getValue() + digit);
                 ++power;
             }
         }
 
         bubbleSort(integers);
 
-        for (int integer : integers) {
+        for (Int integer : integers) {
             System.out.println(integer);
         }
     }
 
-    private static void bubbleSort(int[] sortable) {
+    private static void bubbleSort(Int[] sortable) {
 
         for (int i = 0; i < sortable.length; ++i) {
             for (int j = 1; j < sortable.length - i; ++j) {
-                if (sortable[j-1] > sortable[j]) {
-                    int tmp = sortable[j-1];
+                if (sortable[j-1].getValue() > sortable[j].getValue()) {
+                    Int tmp = sortable[j-1];
                     sortable[j-1] = sortable[j];
                     sortable[j] = tmp;
                 }
